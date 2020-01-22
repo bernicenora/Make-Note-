067de610265cfc84d2182f63906c4b3a21618c6c
BE <bernicewebdesigner@gmail.com>
2020-01-22 09:51:10 -0700

    Trying to Print Notes on screen on Load of the Window-Splitting array

--- js/script.js
+++ js/script.js
@@ -17,6 +17,9 @@ function eventListeners(){
     
     // Remove a note
     noteList.addEventListener('click', removeNote);
+
+    // Document
+    document.addEventListener('DOMContentLoaded', printNotesFromLocalStorageOnLoad);
     }
 
 //Add a note by filling up the form
@@ -65,11 +68,11 @@ function removeNote(e){
 function addNoteLocalStorage(creator,note){
     
     let notes = getNotesFromStorage(creator,note);
-    
+    //console.log("Notes: "+notes);
     notes.push(creator+','+note);
-    //console.log(notes);
+    
 
-    //Converet the Reminder array into a string
+    //Convert the Reminder array into a string
     localStorage.setItem('notes', JSON.stringify(notes));
 }
 
@@ -79,13 +82,51 @@ function getNotesFromStorage(creator,note){
     let notes;
     let creatorLS;
     const notesLS = localStorage.getItem('notes');
-
+    //console.log("NotesLS: "+notesLS);
     // Get the value, if null is returned, then we create an empty array
     if (notesLS === null){
         notes = [];
     }else{
-        notes = JSON.parse(creatorLS+','+notesLS);
+        notes = JSON.parse(notesLS);
     }
     return notes;
+}
+
+// Prints all the Notes on Load of the Window
+function printNotesFromLocalStorageOnLoad(){
+    let notes = getNotesFromStorage(creator,note);
+    
+    notes.forEach(function(creator,note){
+        // Creating the Remove Button
+        const removeBtn = document.createElement('a');
+        removeBtn.classList = 'remove-note';
+        removeBtn.textContent = "DELETE";
+
+        // Display creator and note on the side as an <div> element
+        const div = document.createElement('div'); //create an element called div to house each note detail
+        const pName = document.createElement('p'); //Create a paragraph element for the Name or Creator
+        const pNote = document.createElement('p'); //Create a paragraph element for the Note
+        const newLine = document.createElement('hr'); // Create a Horizontal Element
+
+        //Separating out the Creator of the Note and the Creator
+        console.log("Notes: "+notes);
+        let notesString = toString(JSON.parse(notes));
+        console.log("Notes from LS: "+notesString);
+        let notesArray = notes.split(',');
+        console.log("notesArray0 :"+notesArray[0]);
+        console.log("notesArray1 :"+notesArray[1]);
+        pName.textContent = "Created By: "+creator;
+        pNote.textContent = note;
+        
+        div.appendChild(newLine);
+        div.appendChild(pNote);
+        //Adding the remove Button to each reminder that is generated
+        div.appendChild(removeBtn);
+        div.appendChild(pName);
+        
+
+        // Append the note to the div element
+        noteList.appendChild(div);
+    });
 }
 
